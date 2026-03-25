6ad51dad2b346b6dc04358e1308ca820e4dc2256
senolcolak <senol@kubedo.com>
2026-03-25 11:19:03 +0100

    feat(shares): add share indicator badges to file and folder items

--- frontend/src/lib/components/files/FileListItem.svelte
+++ frontend/src/lib/components/files/FileListItem.svelte
@@ -131,6 +131,14 @@
 							class="font-semibold text-sm lg:text-base hover:text-primary cursor-pointer truncate"
 						>
 							{item.name}
+							{#if item.is_shared}
+								<span class="badge badge-primary badge-xs ml-2" title="Shared">
+									<svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+									</svg>
+									{#if item.share_count && item.share_count > 1}{item.share_count}{/if}
+								</span>
+							{/if}
 						</h3>
 					</button>
 				{:else}
@@ -139,6 +147,14 @@
 							class="font-semibold text-sm lg:text-base hover:text-primary cursor-pointer truncate"
 						>
 							{item.name}
+							{#if item.is_shared}
+								<span class="badge badge-primary badge-xs ml-2" title="Shared">
+									<svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+									</svg>
+									{#if item.share_count && item.share_count > 1}{item.share_count}{/if}
+								</span>
+							{/if}
 						</h3>
 					</button>
 				{/if}
