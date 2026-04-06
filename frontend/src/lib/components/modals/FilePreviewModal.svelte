88aef33a3c2022e0e6987e0947a1bcda90a88bb4
zoorpha <aaron@kubedo.com>
2026-04-06 21:01:41 +0200

    fix: add null check for file before rendering OfficePreview

--- frontend/src/lib/components/modals/FilePreviewModal.svelte
+++ frontend/src/lib/components/modals/FilePreviewModal.svelte
@@ -217,7 +217,7 @@
 						</audio>
 					</div>
 				
-				{:else if capabilities?.previewType === 'office'}
+				{:else if capabilities?.previewType === 'office' && file}
 					<OfficePreview {file}>
 						<button slot="download-button" class="btn btn-primary" on:click={handleDownload}>
 							<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
